cc87caf8b3e89dc649483170de03a694ac219f48
yiweizhou <490493499@qq.com>
2023-08-18 18:49:38 +0800

    配置方案的实现

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"gohub/bootstrap"
+	btsConfig "gohub/config"
+	"gohub/pkg/config"
+
+	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
 func main() {
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
 	router := gin.New()
+
 	bootstrap.SetupRoute(router)
-	err := router.Run(":3000")
+
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
