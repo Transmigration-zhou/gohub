9189e98a64849ec8c88635fb0b363807e0af94d5
yiweizhou <490493499@qq.com>
2023-08-21 18:05:33 +0800

    feat. Panic Recovery

---
+++ app/http/middlewares/recovery.go
@@ -0,0 +1,56 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"gohub/pkg/logger"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"strings"
+	"time"
+)
+
+// Recovery 使用 zap.Error() 来记录 Panic 和 call stack
+func Recovery() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				httpRequest, _ := httputil.DumpRequest(c.Request, true)
+				var brokenPipe bool
+				if ne, ok := err.(*net.OpError); ok {
+					if se, ok := ne.Err.(*os.SyscallError); ok {
+						errStr := strings.ToLower(se.Error())
+						if strings.Contains(errStr, "broken pipe") || strings.Contains(errStr, "connection reset by peer") {
+							brokenPipe = true
+						}
+					}
+				}
+				// 链接中断的情况
+				if brokenPipe {
+					logger.Error(c.Request.URL.Path,
+						zap.Time("time", time.Now()),
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+					c.Error(err.(error))
+					c.Abort()
+					return
+				}
+
+				logger.Error("recovery from panic",
+					zap.Time("time", time.Now()),
+					zap.Any("error", err),
+					zap.String("request", string(httpRequest)),
+					zap.Stack("stacktrace"),
+				)
+
+				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+					"message": "服务器内部错误，请稍后再试",
+				})
+			}
+		}()
+		c.Next()
+	}
+}
