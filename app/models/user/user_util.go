db3949f9e2be826d09d73fe55e33c64f7100d302
yiweizhou <490493499@qq.com>
2023-08-23 20:03:25 +0800

    feat. Auth 中间件

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -31,3 +31,9 @@ func GetByMulti(loginID string) (userModel User) {
 		First(&userModel)
 	return
 }
+
+// GetByID 通过 ID 获取用户
+func GetByID(id string) (userModel User) {
+	database.DB.Where("id", id).First(&userModel)
+	return
+}
