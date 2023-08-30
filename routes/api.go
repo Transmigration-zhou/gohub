a50930de9ea31286287efc3416ae501f468445ff
yiweizhou <490493499@qq.com>
2023-08-30 17:34:48 +0800

    feat. 创建话题

--- routes/api.go
+++ routes/api.go
@@ -57,6 +57,13 @@ func RegisterAPIRoutes(r *gin.Engine) {
 				cgcGroup.PUT("/:id", middlewares.AuthJWT(), cgc.Update)
 				cgcGroup.DELETE("/:id", middlewares.AuthJWT(), cgc.Delete)
 			}
+
+			// 话题
+			tpc := new(controllers.TopicsController)
+			tpcGroup := v1.Group("/topics")
+			{
+				tpcGroup.POST("", middlewares.AuthJWT(), tpc.Store)
+			}
 		}
 	}
 }
